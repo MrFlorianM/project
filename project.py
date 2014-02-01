f0b26700f722048685517ddd5b13969a1934e192
JeLe <dev.lecarpentier@gmail.com>
2014-02-01 22:27:45 +0100

    adding .py file

---
+++ project.py
@@ -0,0 +1,173 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import sys
+
+
+ESCAPE = '\033'
+# Number of the glut window.
+window = 0
+alpha = 0
+
+# all my classes here, and then the instanciation of my first objects !
+
+
+class wall (object):
+    def __init__(self):
+        True
+    
+    def getWallVertices(self, thingToWall):
+        thingToWall.getQuadVertices()
+        
+        self.vertices = []
+        for vertex in thingToWall.vertices:
+            self.vertices.append([vertex[0], 0.1, vertex[2]])
+#z2-z0, z3-z1
+    def checkNotOnWall(self):
+        self.equation1 = (self.vertices[2][2]-self.vertices[0][2])/(self.vertices[2][0]-self.vertices[0][0])
+
+
+class quad(object):
+    def __init__(self, name, Ax, Ay, Az, Vx, Vy, Vz, Wx, Wy, Wz, red, green, blue):
+        self.name = name
+        self.Ax = Ax
+        self.Ay = Ay
+        self.Az = Az
+        self.Vx = Vx
+        self.Vy = Vy
+        self.Vz = Vz
+        self.Wx = Wx
+        self.Wy = Wy
+        self.Wz = Wz
+        self.red = red
+        self.green = green
+        self.blue = blue
+        #I ommited the creation of the vertices here because they might need to be regurlarly updated...
+        self.wall = wall()
+        
+    def getQuadVertices(self):
+        self.vertices = [[self.Ax, self.Ay, self.Az], [self.Ax+self.Vx, self.Ay+self.Vy, self.Az+self.Vz],[self.Ax+self.Vx+self.Wx, self.Ay+self.Vy+self.Wy, self.Az+self.Vz+self.Wz], [self.Ax+self.Wx, self.Ay+self.Wy, self.Az+self.Wz]]
+
+    def drawQuad(self):
+        self.getQuadVertices()
+        glBegin(GL_QUADS)
+        glColor3f(self.red, self.green, self.blue)
+        for vertex in self.vertices:
+            glVertex3f(vertex[0], vertex[1], vertex[2])
+        glEnd()
+
+        self.wall.getWallVertices(self)
+        if self.name != "floor":
+            glBegin(GL_QUADS)
+            glColor3f(0., 1., 0.)
+            for vertex in self.wall.vertices:
+                glVertex3f(vertex[0], vertex[1], vertex[2])
+            glEnd()
+
+
+
+quads= [ quad("floor", 0.0, 0.0, 0.0, 0., 0., -1., 1., 0., 0., 1.0, 1., 0.),
+        quad("wallTest", 0.3, 0.0, -0.3, 0.3, 0.5, -0.0, 0.6, 0., -0.5, 1.0, 0.5, 0.)#        quad("wallTest", 0.0, 0.0, -0.1, 0.5, 0.4, -0.2, 0.2, 0.3, -0.8, 0.5, 0.5, 0.5),        quad("wallTest", 0.2, 0.0, -0.7, 0.1, 0.6, 0.1, 0.4, 0.7, -0.3, 0.3, 0., 0.8)
+        ]
+
+
+
+# A general OpenGL initialization function.  Sets all of the initial parameters.
+def InitGL(Width, Height):				# We call this right after our OpenGL window is created.
+    glClearColor(0.0, 0.0, 1.0, 0.0)	# This Will Clear The Background Color To Black
+    glClearDepth(1.0)					# Enables Clearing Of The Depth Buffer
+    glDepthFunc(GL_LESS)				# The Type Of Depth Test To Do
+    glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
+    glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
+    
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()					# Reset The Projection Matrix
+    # Calculate The Aspect Ratio Of The Window
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    
+    glMatrixMode(GL_MODELVIEW)
+    
+    # The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
+def ReSizeGLScene(Width, Height):
+    if Height == 0:						# Prevent A Divide By Zero If The Window Is Too Small 
+        Height = 1
+
+    glViewport(0, 0, Width, Height)		# Reset The Current Viewport And Perspective Transformation
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+    
+    
+
+
+# The main drawing function. 
+def DrawGLScene():
+    # Clear The Screen And The Depth Buffer, load the current and only matrix
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    
+    # push the origin of (x, y, z) to wher you can see it
+    glTranslatef(-0.5, -0.3, -1.0)
+
+    # my rotation of the whole world to know what i'm doing...
+    global alpha
+    glRotatef(alpha, 0, 1, 0)
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_BYTE, newindices)
+
+
+
+    
+    glutSwapBuffers()
+
+
+
+# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
+def keyPressed(*args):
+    global window
+    global alpha
+    # If escape or q is pressed, kill everything.
+    if args[0] == ESCAPE or args[0] == 'q':
+        sys.exit()
+    if args[0] == 'm':
+        alpha += -1.
+    if args[0] == 'k':
+        alpha += 1.
+
+def main():
+    global window
+
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(640, 480)
+    glutInitWindowPosition(100, 50)
+    window = glutCreateWindow("Lucile's Dude :)")
+    InitGL(640, 480)
+    
+    
+    glutDisplayFunc(DrawGLScene)
+    
+    #glutFullScreen()
+    
+    # these are the callbacks to the functions that actually do something...
+    glutIdleFunc(DrawGLScene)
+    glutReshapeFunc(ReSizeGLScene)
+    glutKeyboardFunc(keyPressed)
+    
+    
+    glutMainLoop()
+    
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+		
